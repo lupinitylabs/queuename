7095e8a66b19b07826350e5a6184937c1a437da6
Oliver Matla <info@lupinitylabs.com>
2020-05-13 02:37:54 +0200

    Add HomepageShown event and Listener.

---
+++ app/Listeners/QueuesOnSomeQueue.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+
+class QueuesOnSomeQueue implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue;
+
+    /**
+     * @var string
+     */
+    public $queue = 'initial-queue';
+
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->queue = 'destination-queue';
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  object  $event
+     * @return void
+     */
+    public function handle($event)
+    {
+        dd($this->job);
+    }
+}
